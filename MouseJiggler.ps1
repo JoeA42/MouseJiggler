b34e8b115e71d61f31a1c0e2cd07ac9e2ed124b4
Jose Alfredo Alvarez Vega <36778393+JoeA42@users.noreply.github.com>
2023-10-13 17:04:32 -0600

    Create MouseJiggler.ps1

---
+++ MouseJiggler.ps1
@@ -0,0 +1,15 @@
+# MouseJiggler Powershell Script
+
+Clear-Host
+Echo "Keep-alive with Scroll Lock..."
+
+$WShell = New-Object -com "Wscript.Shell"
+"Wscript.Shell"
+
+while ($true)
+{
+ $WShell.sendkeys("{SCROLLLOCK}")
+ Start-Sleep -Milliseconds 100
+ $WShell.sendkeys("{SCROLLLOCK}")
+ Start-Sleep -Seconds 240
+}
